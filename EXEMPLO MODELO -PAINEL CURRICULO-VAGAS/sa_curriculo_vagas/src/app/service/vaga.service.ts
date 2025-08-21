f92ee707ade2431931009f3626ff6ce5612a669c
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-08-21 16:04:01 -0300

    21/08/25 - SERVICES ATUALIZADOS

--- EXEMPLO MODELO -PAINEL CURRICULO-VAGAS/sa_curriculo_vagas/src/app/service/vaga.service.ts
+++ EXEMPLO MODELO -PAINEL CURRICULO-VAGAS/sa_curriculo_vagas/src/app/service/vaga.service.ts
@@ -1,35 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Vaga } from '../models/vaga.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class VagaService {
-  private apiUrl = 'http://localhost:3009/vagas'; // JSON Server ou backend
+export class VagasService {
+  private apiUrl = 'http://localhost:3010/vagas'; // URL do json-server
 
   constructor(private http: HttpClient) {}
 
-  // ➝ Listar todas as vagas
-  getVagas(): Observable<Vaga[]> {
-    return this.http.get<Vaga[]>(this.apiUrl);
+  // Criar Vaga
+  createVaga(vaga: any): Observable<any> {
+    return this.http.post(this.apiUrl, vaga);
   }
 
-  // ➝ Cadastrar vaga (Admin)
-  cadastrarVaga(vaga: Vaga): Observable<Vaga> {
-    return this.http.post<Vaga>(this.apiUrl, vaga);
+  // Editar Vaga
+  editVaga(id: number, vaga: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, vaga);
   }
 
-  // ➝ Atualizar vaga (Admin)
-  atualizarVaga(id: number, vaga: Vaga): Observable<Vaga> {
-    const urlAtualizar = `${this.apiUrl}/${id}`;
-    return this.http.put<Vaga>(urlAtualizar, vaga);
+  // Excluir Vaga
+  deleteVaga(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
-  // ➝ Remover vaga (Admin)
-  removerVaga(id: number): Observable<void> {
-    const urlDeletar = `${this.apiUrl}/${id}`;
-    return this.http.delete<void>(urlDeletar);
+  // Listar Vagas
+  getVagas(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  // Visualizar Vaga Específica
+  getVaga(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 }
