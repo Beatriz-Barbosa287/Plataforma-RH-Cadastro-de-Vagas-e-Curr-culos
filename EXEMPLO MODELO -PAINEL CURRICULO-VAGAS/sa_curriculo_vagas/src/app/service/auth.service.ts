223ef1cf9959e037ed1f2523bfdf336231e2b69b
JPcardosoo <joaopedrocardoso08@gmail.com>
2025-08-21 16:47:03 -0300

    21.08.25 "Paginas Login e Cadastro"

--- EXEMPLO MODELO -PAINEL CURRICULO-VAGAS/sa_curriculo_vagas/src/app/service/auth.service.ts
+++ EXEMPLO MODELO -PAINEL CURRICULO-VAGAS/sa_curriculo_vagas/src/app/service/auth.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class AuthService {
+export class UsuarioService {
   private apiUrl = 'http://localhost:3000/usuarios';
 
   constructor(private http: HttpClient) {}
 
-  login(email: string, senha: string) {
-    return this.http.get<any[]>(`${this.apiUrl}?email=${email}&senha=${senha}`);
+  cadastrar(usuario: any): Observable<any> {
+    return this.http.post(this.apiUrl, usuario);
   }
 
-  cadastrar(usuario: any) {
-    return this.http.post(this.apiUrl, usuario);
+  login(email: string, senha: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}?email=${email}&senha=${senha}`);
   }
 }
